39c459ff6bdb1be5660a3cdd83bfa9cc6f23123b
ricardo <ricardoamaral.sistemas@gmail.com>
2024-04-24 08:44:10 -0300

    commit 24 04 2024

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom'; // Importe o ReactDOM corretamente
 
-import './index.css'
-import Home from './pages/Home'
-import Header from './components/Header'
+import './index.css';
+import Home from './pages/Home';
+import Header from './components/Header';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+/*ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-  <Header/>
-   <Home/>
+    <Header />
+    <Home />
   </React.StrictMode>,
-)
+);*/
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Header />
+    <Home />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
